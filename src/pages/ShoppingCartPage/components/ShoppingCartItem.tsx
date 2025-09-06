c09723aa4ad3f2bf09eb706aa77435a6e67cb2ef
sungun <gorjs3540@gmail.com>
2025-09-07 02:26:53 +0900

    refactor: ShoppingCartItem compound 구조로 원복

--- src/pages/ShoppingCartPage/components/ShoppingCartItem.tsx
+++ src/pages/ShoppingCartPage/components/ShoppingCartItem.tsx
@@ -1,52 +1,5 @@
-import { useGetExchangeRate } from '@/model/exchange';
-import { useGetProduct } from '@/model/product';
-import { useCartStore, useHomeStore } from '@/stores';
-import { Counter, Tag, Text, XIcon, type TagType, type TextProps } from '@/ui-lib';
-import { convertPrice } from '@/utils/exchangeRate';
+import { Tag, Text, XIcon, type TagType, type TextProps } from '@/ui-lib';
 import { Center, Flex, styled, type FlexProps } from 'styled-system/jsx';
-
-interface ShoppingCartItemProps {
-  productId: number;
-}
-
-export default function ShoppingCartItem({ productId }: ShoppingCartItemProps) {
-  const currentCurrency = useHomeStore(state => state.currency);
-  const { cartItems, increaseCartItemQuantity, decreaseCartItemQuantity, removeCartItem } = useCartStore(
-    state => state
-  );
-
-  const { data: product } = useGetProduct({ productId });
-  const { data: exchangeRate } = useGetExchangeRate();
-
-  if (!product) return null;
-
-  const cartQuantity = cartItems.find(item => item.productId === productId)?.quantity || 0;
-  const isMaxQuantity = cartQuantity >= product.stock;
-
-  return (
-    <ShoppingCartItemRoot>
-      <ShoppingCartItemImage src={product.images[0]} alt={product.name} />
-      <ShoppingCartItemContent>
-        <ShoppingCartItemInfo
-          type={product.category.toLowerCase() as TagType}
-          title={product.name}
-          description={product.description}
-          onDelete={() => removeCartItem(productId)}
-        />
-        <ShoppingCartItemFooter>
-          <ShoppingCartItemPrice>
-            {convertPrice(product.price, currentCurrency, exchangeRate?.exchangeRate)}
-          </ShoppingCartItemPrice>
-          <Counter.Root>
-            <Counter.Minus onClick={() => decreaseCartItemQuantity(productId)} />
-            <Counter.Display value={cartQuantity} />
-            <Counter.Plus onClick={() => increaseCartItemQuantity(productId)} disabled={isMaxQuantity} />
-          </Counter.Root>
-        </ShoppingCartItemFooter>
-      </ShoppingCartItemContent>
-    </ShoppingCartItemRoot>
-  );
-}
 
 const ShoppingCartItemRoot = ({ children, ...props }: FlexProps) => {
   return (
@@ -109,3 +62,15 @@ const ShoppingCartItemFooter = ({ children, ...props }: FlexProps) => {
     </Flex>
   );
 };
+
+// Compound component pattern
+const ShoppingCartItem = {
+  Root: ShoppingCartItemRoot,
+  Image: ShoppingCartItemImage,
+  Content: ShoppingCartItemContent,
+  Info: ShoppingCartItemInfo,
+  Price: ShoppingCartItemPrice,
+  Footer: ShoppingCartItemFooter,
+};
+
+export default ShoppingCartItem;
