56d43fda285fb0e30832b51e1cd8f0d4d0b2ffe9
sungun <gorjs3540@gmail.com>
2025-09-07 02:46:05 +0900

    feat: CheckoutSection 구현

--- src/pages/ShoppingCartPage/components/CheckoutSection.tsx
+++ src/pages/ShoppingCartPage/components/CheckoutSection.tsx
@@ -1,77 +1,89 @@
-import { SECOND } from "@/constants/time";
-import { Button, Spacing, Text } from "@/ui-lib";
-import { toast } from "@/ui-lib/components/toast";
-import { delay } from "@/utils/async";
-import { useState } from "react";
-import { useNavigate } from "react-router";
-import { Box, Divider, Flex, HStack, Stack, styled } from "styled-system/jsx";
+import { DELIVERY_FEE_MAP } from '@/constants/delivery';
+import { SECOND } from '@/constants/time';
+import { useGetExchangeRate } from '@/model/exchange';
+import { useGetCartProductList } from '@/model/product';
+import { useGetUser } from '@/model/user';
+import { useHomeStore } from '@/stores';
+import { Button, Spacing, Text } from '@/ui-lib';
+import { toast } from '@/ui-lib/components/toast';
+import { delay } from '@/utils/async';
+import { convertPrice } from '@/utils/exchangeRate';
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import { Box, Divider, Flex, HStack, Stack, styled } from 'styled-system/jsx';
 
 function CheckoutSection() {
-	const navigate = useNavigate();
-	const [isPurchasing, setIsPurchasing] = useState(false);
+  const navigate = useNavigate();
 
-	const onClickPurchase = async () => {
-		setIsPurchasing(true);
-		await delay(SECOND * 1);
-		setIsPurchasing(false);
-		toast.success("결제가 완료되었습니다.");
-		await delay(SECOND * 2);
-		navigate("/");
-	};
+  const [isPurchasing, setIsPurchasing] = useState(false);
+  const currentCurrency = useHomeStore(state => state.currency);
 
-	return (
-		<styled.section css={{ p: 5, bgColor: "background.01_white" }}>
-			<Text variant="H2_Bold">결제금액</Text>
+  const { data: user } = useGetUser();
+  const { data: exchangeRate } = useGetExchangeRate();
+  const { cartProducts, totalPrice } = useGetCartProductList();
 
-			<Spacing size={4} />
+  const onClickPurchase = async () => {
+    setIsPurchasing(true);
+    await delay(SECOND * 1);
+    setIsPurchasing(false);
+    toast.success('결제가 완료되었습니다.');
+    await delay(SECOND * 2);
+    navigate('/');
+  };
 
-			<Stack
-				gap={6}
-				css={{
-					p: 5,
-					border: "1px solid",
-					borderColor: "border.01_gray",
-					rounded: "2xl",
-				}}
-			>
-				<Stack gap={5}>
-					<Box gap={3}>
-						<Flex justify="space-between">
-							<Text variant="B2_Regular">주문금액(3개)</Text>
-							<Text variant="B2_Bold" color="state.green">
-								무료배송
-							</Text>
-						</Flex>
-						<Spacing size={3} />
-						<Flex justify="space-between">
-							<Text variant="B2_Regular">배송비</Text>
-							<Text variant="B2_Bold">무료</Text>
-						</Flex>
-					</Box>
+  const deliveryFee = totalPrice > 30 ? 0 : DELIVERY_FEE_MAP[user?.grade as Grade];
+  const isFreeDelivery = deliveryFee === 0;
 
-					<Divider color="border.01_gray" />
+  return (
+    <styled.section css={{ p: 5, bgColor: 'background.01_white' }}>
+      <Text variant="H2_Bold">결제금액</Text>
 
-					<HStack justify="space-between">
-						<Text variant="H2_Bold">총 금액</Text>
-						<Text variant="H2_Bold">$30.59</Text>
-					</HStack>
-				</Stack>
+      <Spacing size={4} />
 
-				<Button
-					fullWidth
-					size="lg"
-					loading={isPurchasing}
-					onClick={onClickPurchase}
-				>
-					{isPurchasing ? "결제 중..." : "결제 진행"}
-				</Button>
+      <Stack
+        gap={6}
+        css={{
+          p: 5,
+          border: '1px solid',
+          borderColor: 'border.01_gray',
+          rounded: '2xl',
+        }}
+      >
+        <Stack gap={5}>
+          <Box gap={3}>
+            <Flex justify="space-between">
+              <Text variant="B2_Regular">주문금액({cartProducts.length}개)</Text>
+              <Text variant="B2_Bold">{convertPrice(totalPrice, currentCurrency, exchangeRate?.exchangeRate)}</Text>
+            </Flex>
+            <Spacing size={3} />
+            <Flex justify="space-between">
+              <Text variant="B2_Regular">배송비</Text>
+              <Text variant="B2_Bold" color={isFreeDelivery ? 'state.green' : 'neutral.01_black'}>
+                {isFreeDelivery ? '무료 배송' : convertPrice(deliveryFee, currentCurrency, exchangeRate?.exchangeRate)}
+              </Text>
+            </Flex>
+          </Box>
 
-				<Text variant="C2_Regular" color="neutral.03_gray">
-					{`우리는 신용카드, 은행 송금, 모바일 결제, 현금을 받아들입니다\n안전한 체크아웃\n귀하의 결제 정보는 암호화되어 안전합니다.`}
-				</Text>
-			</Stack>
-		</styled.section>
-	);
+          <Divider color="border.01_gray" />
+
+          <HStack justify="space-between">
+            <Text variant="H2_Bold">총 금액</Text>
+            <Text variant="H2_Bold">
+              {convertPrice(totalPrice + deliveryFee, currentCurrency, exchangeRate?.exchangeRate)}
+            </Text>
+          </HStack>
+        </Stack>
+
+        <Button fullWidth size="lg" loading={isPurchasing} onClick={onClickPurchase}>
+          {isPurchasing ? '결제 중...' : '결제 진행'}
+        </Button>
+
+        <Text variant="C2_Regular" color="neutral.03_gray">
+          {`우리는 신용카드, 은행 송금, 모바일 결제, 현금을 받아들입니다\n안전한 체크아웃\n귀하의 결제 정보는 암호화되어 안전합니다.`}
+        </Text>
+      </Stack>
+    </styled.section>
+  );
 }
 
 export default CheckoutSection;
